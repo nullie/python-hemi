44c543c193f75060fc55143b6707d593bbaaba82
Ilya Novoselov <ilya.novoselov@gmail.com>
2011-11-05 04:36:37 +0600

    Refactoring

--- hemi.cpp
+++ hemi.cpp
@@ -1,15 +1,6 @@
 // -*- c-file-style: "cc-mode" -*-
 
-#include <Python.h>
-#include <v8.h>
-
-PyObject* wrap(v8::Handle<v8::Context> context, v8::Handle<v8::Value> value);
-
-typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    v8::Persistent<v8::Context> context;
-} Context;
+#include "hemi.h"
 
 extern "C" void Context_dealloc(Context* self) {
     self->context.Dispose();
@@ -54,7 +45,7 @@ extern "C" int Context_init(Context *self, PyObject *args, PyObject *kwds) {
     return 0;
 }
 
-extern "C" PyObject* Context_getglobals(Context *self, void *closure) {
+extern "C" PyObject * Context_getglobals(Context *self, void *closure) {
     using namespace v8;
 
     v8::Context::Scope context_scope(self->context);
@@ -68,69 +59,13 @@ extern "C" PyObject* Context_getglobals(Context *self, void *closure) {
     return wrapped;
 }
 
-static PyGetSetDef Context_getseters[] = {
-    {"globals",
-     (getter)Context_getglobals, NULL,
-     "Context globals",
-     NULL},
-    {NULL} /* Sentinel */
-};
-
-static PyTypeObject ContextType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                             /* ob_size */
-    "hemi.Context",                /* tp_name */
-    sizeof(Context),               /* tp_basicsize */
-    0,                             /* tp_itemsize */
-    (destructor)Context_dealloc,   /* tp_dealloc */
-    0,                             /* tp_print */
-    0,                             /* tp_getattr */
-    0,                             /* tp_setattr */
-    0,                             /* tp_compare */
-    0,                             /* tp_repr */
-    0,                             /* tp_as_number */
-    0,                             /* tp_as_sequence */
-    0,                             /* tp_as_mapping */
-    0,                             /* tp_hash */
-    0,                             /* tp_call */
-    0,                             /* tp_str */
-    0,                             /* tp_getattro */
-    0,                             /* tp_setattro */
-    0,                             /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,            /* tp_flags */
-    "Interpreter context",         /* tp_doc */
-    0,		                       /* tp_traverse */
-    0,		                       /* tp_clear */
-    0,		                       /* tp_richcompare */
-    0,		                       /* tp_weaklistoffset */
-    0,		                       /* tp_iter */
-    0,		                       /* tp_iternext */
-    0,                             /* tp_methods */
-    0,                             /* tp_members */
-    Context_getseters,             /* tp_getset */
-    0,                             /* tp_base */
-    0,                             /* tp_dict */
-    0,                             /* tp_descr_get */
-    0,                             /* tp_descr_set */
-    0,                             /* tp_dictoffset */
-    (initproc)Context_init,        /* tp_init */
-    0,                             /* tp_alloc */
-};
-
-typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    v8::Persistent<v8::Context> context;
-    v8::Persistent<v8::Object> object;
-} Object;
-
 extern "C" void Object_dealloc(Object* self) {
     self->object.Dispose();
 
     self->ob_type->tp_free((PyObject *)self);
 }
 
-extern "C" PyObject* Object_getitem(Object *self, PyObject *item) {
+extern "C" PyObject * Object_getitem(Object *self, PyObject *item) {
     using namespace v8;
 
     if(PyUnicode_Check(item)) {
@@ -150,7 +85,7 @@ extern "C" PyObject* Object_getitem(Object *self, PyObject *item) {
     return NULL;
 };
 
-extern "C" PyObject* Object_getattr(Object *self, PyObject *name) {
+extern "C" PyObject * Object_getattr(Object *self, PyObject *name) {
     using namespace v8;
 
     PyObject *value = PyObject_GenericGetAttr((PyObject *)self, name);
@@ -174,37 +109,7 @@ extern "C" PyObject* Object_getattr(Object *self, PyObject *name) {
     return Py_BuildValue("d", result->NumberValue());
 };
 
-static PyMappingMethods Object_as_mapping = {
-    0,
-    (binaryfunc)Object_getitem,
-};
-
-static PyTypeObject ObjectType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                            /* ob_size */
-    "hemi.Object",                /* tp_name */
-    sizeof(Object),               /* tp_basicsize */
-    0,                            /* tp_itemsize */
-    (destructor)Object_dealloc,   /* tp_dealloc */
-    0,                            /* tp_print */
-    0,                            /* tp_getattr */
-    0,                            /* tp_setattr */
-    0,                            /* tp_compare */
-    0,                            /* tp_repr */
-    0,                            /* tp_as_number */
-    0,                            /* tp_as_sequence */
-    &Object_as_mapping,           /* tp_as_mapping */
-    0,                            /* tp_hash */
-    0,                            /* tp_call */
-    0,                            /* tp_str */
-    (getattrofunc)Object_getattr, /* tp_getattro */
-    0,                            /* tp_setattro */
-    0,                            /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,           /* tp_flags */
-    "Javascript object",          /* tp_doc */
-};
-
-PyObject* wrap(v8::Handle<v8::Context> context, v8::Handle<v8::Value> value) {
+PyObject * wrap(v8::Handle<v8::Context> context, v8::Handle<v8::Value> value) {
     PyObject *rv;
 
     if(value->IsNumber()) {
@@ -220,10 +125,6 @@ PyObject* wrap(v8::Handle<v8::Context> context, v8::Handle<v8::Value> value) {
 
     return rv;
 }
-
-static PyMethodDef module_methods[] = {
-    {NULL}  /* Sentinel */
-};
 
 #ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
 #define PyMODINIT_FUNC void
---
+++ hemi.h
@@ -0,0 +1,110 @@
+// -*- c-file-style: "cc-mode" -*-
+
+#include <Python.h>
+#include <v8.h>
+
+typedef struct {
+    PyObject_HEAD
+    /* Type-specific fields go here. */
+    v8::Persistent<v8::Context> context;
+} Context;
+
+extern "C" void Context_dealloc(Context* self);
+extern "C" int Context_init(Context *self, PyObject *args, PyObject *kwds);
+extern "C" PyObject* Context_getglobals(Context *self, void *closure);
+
+static PyGetSetDef Context_getseters[] = {
+    {(char *)"globals",
+     (getter)Context_getglobals, NULL,
+     (char *)"Context globals",
+     NULL},
+    {NULL} /* Sentinel */
+};
+
+static PyTypeObject ContextType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                             /* ob_size */
+    "hemi.Context",                /* tp_name */
+    sizeof(Context),               /* tp_basicsize */
+    0,                             /* tp_itemsize */
+    (destructor)Context_dealloc,   /* tp_dealloc */
+    0,                             /* tp_print */
+    0,                             /* tp_getattr */
+    0,                             /* tp_setattr */
+    0,                             /* tp_compare */
+    0,                             /* tp_repr */
+    0,                             /* tp_as_number */
+    0,                             /* tp_as_sequence */
+    0,                             /* tp_as_mapping */
+    0,                             /* tp_hash */
+    0,                             /* tp_call */
+    0,                             /* tp_str */
+    0,                             /* tp_getattro */
+    0,                             /* tp_setattro */
+    0,                             /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,            /* tp_flags */
+    "Interpreter context",         /* tp_doc */
+    0,		                       /* tp_traverse */
+    0,		                       /* tp_clear */
+    0,		                       /* tp_richcompare */
+    0,		                       /* tp_weaklistoffset */
+    0,		                       /* tp_iter */
+    0,		                       /* tp_iternext */
+    0,                             /* tp_methods */
+    0,                             /* tp_members */
+    Context_getseters,             /* tp_getset */
+    0,                             /* tp_base */
+    0,                             /* tp_dict */
+    0,                             /* tp_descr_get */
+    0,                             /* tp_descr_set */
+    0,                             /* tp_dictoffset */
+    (initproc)Context_init,        /* tp_init */
+    0,                             /* tp_alloc */
+};
+
+typedef struct {
+    PyObject_HEAD
+    /* Type-specific fields go here. */
+    v8::Persistent<v8::Context> context;
+    v8::Persistent<v8::Object> object;
+} Object;
+
+extern "C" void Object_dealloc(Object* self);
+extern "C" PyObject* Object_getitem(Object *self, PyObject *item);
+extern "C" PyObject* Object_getattr(Object *self, PyObject *name);
+
+static PyMappingMethods Object_as_mapping = {
+    0,
+    (binaryfunc)Object_getitem,
+};
+
+static PyTypeObject ObjectType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                            /* ob_size */
+    "hemi.Object",                /* tp_name */
+    sizeof(Object),               /* tp_basicsize */
+    0,                            /* tp_itemsize */
+    (destructor)Object_dealloc,   /* tp_dealloc */
+    0,                            /* tp_print */
+    0,                            /* tp_getattr */
+    0,                            /* tp_setattr */
+    0,                            /* tp_compare */
+    0,                            /* tp_repr */
+    0,                            /* tp_as_number */
+    0,                            /* tp_as_sequence */
+    &Object_as_mapping,           /* tp_as_mapping */
+    0,                            /* tp_hash */
+    0,                            /* tp_call */
+    0,                            /* tp_str */
+    (getattrofunc)Object_getattr, /* tp_getattro */
+    0,                            /* tp_setattro */
+    0,                            /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,           /* tp_flags */
+    "Javascript object",          /* tp_doc */
+};
+
+PyObject* wrap(v8::Handle<v8::Context> context, v8::Handle<v8::Value> value);
+
+static PyMethodDef module_methods[] = {
+    {NULL}  /* Sentinel */
+};
