7d6a315e270a28a700cac50c14d75cd56371bf31
Ilya Novoselov <nullie@yandex-team.ru>
2012-10-02 17:39:40 +0000

    Submitted package to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
 
 setup(
-    name="hemi", version="0.2.5",
+    name="hemi",
+    version="0.2.5",
+    url="https://github.com/nullie/python-hemi",
+    author="Ilya Novoselov",
+    author_email="ilya.novoselov@gmail.com",
     ext_modules=[
         Extension(
             "hemi",
